5fa45069717e2e0fb159c6f3c24a245e401196b3
benjamin-braun-hv <benjamin.braun@hansevision.de>
2025-04-08 09:07:32 +0000

    matomo

--- matomo-haiilo.js
+++ matomo-haiilo.js
@@ -1,11 +1,55 @@
-var _paq = window._paq = window._paq || [];
-/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-_paq.push(['trackPageView']);
-_paq.push(['enableLinkTracking']);
 (function () {
-    var u = "https://hansevision.matomo.cloud/";
-    _paq.push(['setTrackerUrl', u + 'matomo.php']);
-    _paq.push(['setSiteId', '1']);
-    var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
-    g.async = true; g.src = 'https://cdn.matomo.cloud/hansevision.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g, s);
+    var matomoInstance = "hansevision.matomo.cloud";
+    var matomoBaseUrl = "https://" + matomoInstance + "/";
+    var matomoPhpUrl = matomoBaseUrl + "matomo.php";
+    var matomoJsUrl = "https://cdn.matomo.cloud/" + matomoInstance + "/matomo.js";
+    var matomoScriptId = "matomo-script";
+
+    // Initialize Matomo tracking
+    function initializeMatomo() {
+        var matomoQueue = window._paq = window._paq || [];
+        matomoQueue.push(['trackPageView']);
+        matomoQueue.push(['enableLinkTracking']);
+        matomoQueue.push(['setTrackerUrl', matomoPhpUrl]);
+        matomoQueue.push(['setSiteId', '1']);
+    }
+
+    // Method to track page events
+    function trackPageEvent(pageUrl) {
+        var matomoQueue = window._paq = window._paq || [];
+        matomoQueue.push(['setCustomUrl', pageUrl]);
+        matomoQueue.push(['trackPageView']);
+    }
+
+    // Add the Matomo script to the page
+    function addMatomoScript() {
+        // Check if the script is already added
+        if (document.getElementById(matomoScriptId)) {
+            return;
+        }
+
+        var documentObject = document;
+        var scriptElement = documentObject.createElement('script');
+        scriptElement.type = 'text/javascript';
+        scriptElement.src = matomoJsUrl;
+        scriptElement.async = true;
+        scriptElement.id = matomoScriptId;
+
+        // Append the script just before the first script tag
+        var firstScript = documentObject.getElementsByTagName('script')[0];
+        firstScript.parentNode.insertBefore(scriptElement, firstScript);
+    }
+
+    // Listener for popstate event to track page changes
+    function setupPopstateListener() {
+        window.addEventListener('popstate', function (event) {
+            var currentUrl = window.location.href;
+            trackPageEvent(currentUrl);
+        });
+    }
+
+    // Initialize Matomo, add the script, and set up the listener
+    initializeMatomo();
+    addMatomoScript();
+    setupPopstateListener();
 })();
